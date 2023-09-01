f10efc994268be62b7da153640bf1569258f5287
Ko1da3vN1k1ta <maniac779@mail.ru>
2023-09-01 11:00:56 +0300

    Create const

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        // IntelliJ IDEA suggests fixing it
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -15,5 +14,8 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+    }
+    public class MyMath{
+
     }
 }
---
+++ src/main/java/org/example/MyMath.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MyMath {
+    private static final double p = 3.14;
+    private static final double e = 2.71;
+    public double tangens = 0;
+    public double sinus = 0;
+    public double cosinus = 0;
+
+
+}
