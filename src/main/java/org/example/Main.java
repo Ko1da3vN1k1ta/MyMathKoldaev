08db9c0140c968324f62687ae1e7a8e26b0f6492
Tappochek <57657750+Tappochek@users.noreply.github.com>
2023-09-04 00:21:09 +0300

    I'm done sir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,14 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
-    public class MyMath{
-
+        System.out.println(MyMath.sin(30));
+        System.out.println(MyMath.cos(45));
+        System.out.println(MyMath.tg(60));
+        System.out.println(MyMath.areaCircle(40));
+        System.out.println(MyMath.lengthCircle(55));
+        System.out.println(MyMath.random(3));
     }
 }
--- src/main/java/org/example/MyMath.java
+++ src/main/java/org/example/MyMath.java
@@ -1,11 +1,53 @@
 package org.example;
 
 public class MyMath {
-    private static final double p = 3.14;
-    private static final double e = 2.71;
-    public double tangens = 0;
-    public double sinus = 0;
-    public double cosinus = 0;
+    private static final double PI = 3.141592653589793;
+    private static final double E = 2.71828182845;
+public static double toRadians(double degrees){
 
+    return degrees * (PI/180);
+}
+public static double sin(double degrees){
 
+    double radians = toRadians(degrees);
+    double sin = 0;
+    double progress = radians;
+    for(int i = 1; i <= 10; i++){
+        sin += progress;
+        progress *= -(radians * radians)/((2 + i)*(2 * i + 1));
+    }
+    return sin;
+}
+    public static double sqrt(double number) {
+
+        double t;
+        double squareRoot = number / 2;
+        do {
+            t = squareRoot;
+            squareRoot = (t + (number / t)) / 2;
+        } while ((t - squareRoot) != 0);
+        return squareRoot;
+    }
+    public static double cos(double degrees){
+
+    double sin = sin(degrees);
+    return sqrt(1 - sin * sin);
+    }
+public static double tg(double degrees){
+
+    return sin(degrees)/cos(degrees);
+}
+public static double areaCircle(double radius){
+
+    return PI * (radius * radius);
+}
+public static double lengthCircle(double radius){
+
+return 2 * PI * radius;
+}
+public static int random(long seed) {
+        int bits = 32;
+        seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
+        return (int) (seed >>> (48 - bits));
+    }
 }
