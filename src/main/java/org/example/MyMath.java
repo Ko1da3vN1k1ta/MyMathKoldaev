a124493ee1db3fec64af5716da0a38b1f22869b6
Ko1da3vN1k1ta <maniac779@mail.ru>
2023-09-04 14:25:07 +0300

    Create const Create random And another task

--- src/main/java/org/example/MyMath.java
+++ src/main/java/org/example/MyMath.java
@@ -1,23 +1,28 @@
 package org.example;
 
+import java.util.Random;
+
 public class MyMath {
     private static final double PI = 3.141592653589793;
     private static final double E = 2.71828182845;
-public static double toRadians(double degrees){
 
-    return degrees * (PI/180);
-}
-public static double sin(double degrees){
+    public static double toRadians(double degrees) {
 
-    double radians = toRadians(degrees);
-    double sin = 0;
-    double progress = radians;
-    for(int i = 1; i <= 10; i++){
-        sin += progress;
-        progress *= -(radians * radians)/((2 + i)*(2 * i + 1));
+        return degrees * (PI / 180);
     }
-    return sin;
-}
+
+    public static double sin(double degrees) {
+
+        double radians = toRadians(degrees);
+        double sin = 0;
+        double progress = radians;
+        for (int i = 1; i <= 10; i++) {
+            sin += progress;
+            progress *= -(radians * radians) / ((2 + i) * (2 * i + 1));
+        }
+        return sin;
+    }
+
     public static double sqrt(double number) {
 
         double t;
@@ -28,26 +33,57 @@ public static double sin(double degrees){
         } while ((t - squareRoot) != 0);
         return squareRoot;
     }
-    public static double cos(double degrees){
 
-    double sin = sin(degrees);
-    return sqrt(1 - sin * sin);
+    public static double cos(double degrees) {
+
+        double sin = sin(degrees);
+        return sqrt(1 - sin * sin);
     }
-public static double tg(double degrees){
 
-    return sin(degrees)/cos(degrees);
-}
-public static double areaCircle(double radius){
+    public static double tg(double degrees) {
 
-    return PI * (radius * radius);
-}
-public static double lengthCircle(double radius){
+        return sin(degrees) / cos(degrees);
+    }
 
-return 2 * PI * radius;
-}
-public static int random(long seed) {
-        int bits = 32;
-        seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
-        return (int) (seed >>> (48 - bits));
+    public static double areaCircle(double radius) {
+
+        return PI * (radius * radius);
+    }
+
+    public static double lengthCircle(double radius) {
+
+        return 2 * PI * radius;
+    }
+
+    public static double random(double number) {
+        Random random = new Random();
+        number = random.nextDouble(10);
+        return number;
+
+    }
+
+    public static int maxNumbers(int a, int b) {
+        if(a > b) {
+            return a;
+        } else return b;
+    }
+
+    public static float maxNumbers(float a, float b) {
+        if(a > b) {
+            return a;
+        } else return b;
+    }
+
+    public static double maxNumbers(double a, double b) {
+        if(a > b) {
+            return a;
+        } else return b;
+    }
+
+    public static long maxNumbers(long a, long b) {
+        if(a > b) {
+            return a;
+        } else return b;
     }
 }
+
